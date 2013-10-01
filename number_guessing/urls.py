ea8f6265aa275e9ce4d7a0c9132f7f75c2b99d74
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-10-01 20:50:38 +0200

    Finished first draft of the game
    Added an intermediate view - start_game in which session variables are
    initialised. Created a separate template for the congratulation 
    message.

--- number_guessing/urls.py
+++ number_guessing/urls.py
@@ -8,4 +8,6 @@ from number_guessing import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^play/$', views.play, name='play'))
+                       url(r'^start_game/$', views.start_game, name='start_game'),
+                       url(r'^play/$', views.play, name='play'),
+                       url(r'^success/$', views.success, name='success'))
--- number_guessing/views.py
+++ number_guessing/views.py
@@ -1,18 +1,36 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse, HttpResponseRedirect,\
+    HttpResponseRedirectBase
+
+ATTEMPT_COUNTER = 'ATTEMPT_COUNTER'
+NUMBER_TO_GUESS = 'NUMBER_TO_GUESS' 
+
 
 def index(request):
+    print 'bar'
     return render(request, 'number_guessing/index.html')
 
-def play(request, choice = None):
+def start_game(request):
+    request.session[ATTEMPT_COUNTER] = 0
+    request.session[NUMBER_TO_GUESS] = 2
+    print 'foo'
+    return redirect('/guess/play')
     
-    if 'number' not in request.session:
-        request.session['number'] = 2
+
+def play(request, choice = None):
+    request.session[ATTEMPT_COUNTER] = int(request.session[ATTEMPT_COUNTER]) + 1
+    
+    if request.session[ATTEMPT_COUNTER] == 1:
+        return render(request, 'number_guessing/play.html', {'numbers': range(5)})
     else:
+        print request.session['NUMBER_TO_GUESS']
         selected_choice = request.POST['choice']
     
     
     if int(selected_choice) == 2:
-        return HttpResponse('Hurray! You\'ve won!')
+        return render(request, 'number_guessing/success.html')
     else: 
         return render(request, 'number_guessing/play.html', {'numbers': range(5)})
+    
+def success(request):
+    return render(request, 'number_guessing/success.html')
